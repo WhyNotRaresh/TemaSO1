4b7352b0a84e4aa26c4eb4bc60835e999cb6e011
whynotraresh <myonlybaraoc@gmail.com>
2021-03-15 15:13:28 +0200

    Changes to be committed: 	modified:   PreProcess-Main.c

--- PreProcess-Main.c
+++ PreProcess-Main.c
@@ -6,11 +6,6 @@
 int main(int argc, char* argv[]) {
 	allocHM(20);
 
-	insert("ana", "mere");
-	A_Item it = search("ana");
-	if (it != NULL)
-		printf("%s\n", it->data);
-
 	deallocHM();
 	return 0;
 }
